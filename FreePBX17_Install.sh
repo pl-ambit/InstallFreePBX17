5a4fdbd7988afa73909960e3b72a67ac1239a17b
pl-ambit <philiplindquist@ambit-llc.com>
2024-11-01 10:45:16 -0500

    Add files via upload

---
+++ FreePBX17_Install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://github.com/FreePBX/sng_freepbx_debian_install/raw/master/sng_freepbx_debian_install.sh -O /tmp/sng_freepbx_debian_install.sh
+bash /tmp/sng_freepbx_debian_install.sh
